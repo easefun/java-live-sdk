c33a403fa679fd8c6cc0127962b4687a7d684fd0
黄裕丰 <huangyufeng@polyv.net>
2019-09-03 23:11:13 +0800

    版本分支1.5.1 接口注释完善修改

--- src/main/java/com/polyv/live/service/PLChannelAdvertService.java
+++ src/main/java/com/polyv/live/service/PLChannelAdvertService.java
@@ -15,7 +15,7 @@ public interface PLChannelAdvertService extends PLBaseService {
 
     /**
      * <pre>
-     * 获取频道下的轮播广告列表(详见)
+     * 获取频道下的轮播广告列表(详见http://dev.polyv.net/2019/uncategorized/channel-advert-list/)
      * 该接口提供用户查询频道下轮播广告列表信息。
      * 需要调用该接口的情况：
      * ◆ 业务需要获取频道轮播广告列表用于管理；
