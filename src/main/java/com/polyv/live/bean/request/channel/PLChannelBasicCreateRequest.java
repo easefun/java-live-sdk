ac3aebd14eef13219f8b0ac0f74a41e5ef5d0973
moshunwei <moshunwei@polyv.net>
2019-09-03 16:11:59 +0800

    LIVE-15857 【java sdk】创建频道接口补充请求参数

--- src/main/java/com/polyv/live/bean/request/channel/PLChannelBasicCreateRequest.java
+++ src/main/java/com/polyv/live/bean/request/channel/PLChannelBasicCreateRequest.java
@@ -11,7 +11,32 @@ import com.polyv.live.bean.request.PLBaseRequest;
  */
 public class PLChannelBasicCreateRequest extends PLBaseRequest {
     
+    /**
+     * <pre>
+     * 字段名：创建频道默认值请求体
+     * 变量名：requestBody
+     * 是否必填：是
+     * 类型：PLChannelBasicCreateBody
+     * 示例值：
+     * 描述：创建频道默认值请对象
+     * </pre>
+     */
+    protected PLChannelBasicCreateBody requestBody;
+    
     public PLChannelBasicCreateRequest(String appId, String appSecret) {
         super(appId, appSecret);
+    }
+    
+    public PLChannelBasicCreateRequest(String appId, String appSecret, PLChannelBasicCreateBody requestBody) {
+        super(appId, appSecret);
+        this.requestBody = requestBody;
+    }
+    
+    public PLChannelBasicCreateBody getRequestBody() {
+        return requestBody;
+    }
+    
+    public void setRequestBody(PLChannelBasicCreateBody requestBody) {
+        this.requestBody = requestBody;
     }
 }
--- src/main/java/com/polyv/live/bean/request/channel/PLChannelBasicUpdateRequest.java
+++ src/main/java/com/polyv/live/bean/request/channel/PLChannelBasicUpdateRequest.java
@@ -4,10 +4,38 @@ import com.polyv.live.bean.request.PLBaseRequest;
 
 public class PLChannelBasicUpdateRequest extends PLBaseRequest {
     
-    private Integer channelId;
+    /**
+     * <pre>
+     * 字段名：频道号
+     * 变量名：channelId
+     * 是否必填：是
+     * 类型：Integer
+     * 示例值：100010
+     * 描述：频道号
+     * </pre>
+     */
+    protected Integer channelId;
+    
+    /**
+     * <pre>
+     * 字段名：创建频道默认值请求体
+     * 变量名：requestBody
+     * 是否必填：是
+     * 类型：PLChannelBasicCreateBody
+     * 示例值：
+     * 描述：创建频道默认值请对象
+     * </pre>
+     */
+    protected PLChannelBasicUpdateBody requestBody;
     
     public PLChannelBasicUpdateRequest(String appId, String appSecret) {
         super(appId, appSecret);
+    }
+    
+    public PLChannelBasicUpdateRequest(String appId, String appSecret, Integer channelId, PLChannelBasicUpdateBody requestBody) {
+        super(appId, appSecret);
+        this.channelId = channelId;
+        this.requestBody = requestBody;
     }
     
     public Integer getChannelId() {
@@ -16,5 +44,13 @@ public class PLChannelBasicUpdateRequest extends PLBaseRequest {
     
     public void setChannelId(Integer channelId) {
         this.channelId = channelId;
+    }
+    
+    public PLChannelBasicUpdateBody getRequestBody() {
+        return requestBody;
+    }
+    
+    public void setRequestBody(PLChannelBasicUpdateBody requestBody) {
+        this.requestBody = requestBody;
     }
 }
